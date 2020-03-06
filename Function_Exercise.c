23cddd739c8decc63347281eb89a3d360875b3a6
gtoledorodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2020-03-06 12:06:23 -0600

    Create Function_Exercise.c

---
+++ Function_Exercise.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+//Add Line of Code Here
+
+int main(){
+  // Declare Variables
+  int x; 
+  int y;
+  
+  printf( "Please input two numbers to be multiplied: " ); //Print Statement
+  scanf( "%d", &x ); //Accepts User Input
+  scanf( "%d", &y ); //Accepts User Input
+  printf( "The product of your two numbers is %d\n", mult( x, y ) ); //Print Statement with return of function
+  getchar();
+}
+
+//Add Missing Code Here
+You_Can_Change_Me{
+  //Missing code
+}
+
+
